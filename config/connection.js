5edd23e827a9df3fb2fcfa87f3b5291fcb9fcdd9
rahoofnazer <rahoofnazer@gmail.com>
2021-02-01 20:48:04 +0530

    mongo change

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+    const url='mongodb+srv://cluster0.n7l1e.mongodb.net/askin" --username askin'
     const dbname='askin'
 
     mongoClient.connect(url,(err,data)=>{
